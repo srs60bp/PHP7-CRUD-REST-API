b9ac8a10aa7a5e097465a35e3aece7e97dcb04a5
srs60bp <49392389+srs60bp@users.noreply.github.com>
2019-06-05 10:16:58 +0300

    Create database.sql

---
+++ config/data/database.sql
@@ -0,0 +1,11 @@
+/* You can also use an installer.php script which gets called one time to execute a SQL script 
+/* that creates a database and create the database tables.
+/*
+/* If you want to take this approach, create config/data/database.sql then copy the following code 
+/* plus the previous SQL CREATE statements to create the tables
+*/
+CREATE DATABASE mydb;
+
+use mydb;
+
+/* COPY THE PREVIOUS STATEMENTS HERE*/
